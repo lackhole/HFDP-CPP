0ac9a351fe35a29bd8a3f6c05f96a9f538f52f4f
lackhole <35574936+lackhole@users.noreply.github.com>
2021-02-09 21:42:32 +0900

    clear outside files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Head First Design Patterns (C++)
+
+This implements the examples in *Head First Design Patterns*, written by Eric Freeman, C++ 
---
+++ strategy/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include "duck_core.hpp"
+#include "duck_interface.hpp"
+
+int main() {
+
+
+  return 0;
+}
