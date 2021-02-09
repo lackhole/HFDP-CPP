d4870b8ccfe764103c33ecd5b01ad71c3b96da6a
lackhole <35574936+lackhole@users.noreply.github.com>
2021-02-09 23:20:34 +0900

    add FlyRocketPowered

---
+++ strategy/dynamic_duck/include/dynamic_duck/fly_behavior/fly_rocket_powered.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cosge on 2021-02-09.
+//
+
+#include <iostream>
+#include "fly_rocket_powered.hpp"
+
+void FlyRocketPowered::fly() const {
+  std::cout << "Fly rocket powered" << std::endl;
+}
---
+++ strategy/dynamic_duck/include/dynamic_duck/fly_behavior/fly_rocket_powered.hpp
@@ -0,0 +1,16 @@
+//
+// Created by cosge on 2021-02-09.
+//
+
+#ifndef STRATEGY_DYNAMIC_DUCK_FLY_BEHAVIOR_FLY_ROCKET_POWERED_HPP
+#define STRATEGY_DYNAMIC_DUCK_FLY_BEHAVIOR_FLY_ROCKET_POWERED_HPP
+
+#include "dynamic_duck/fly_behavior/fly_behavior.hpp"
+
+class FlyRocketPowered : public FlyBehavior {
+ public:
+  void fly() const override;
+};
+
+
+#endif //STRATEGY_DYNAMIC_DUCK_FLY_BEHAVIOR_FLY_ROCKET_POWERED_HPP
