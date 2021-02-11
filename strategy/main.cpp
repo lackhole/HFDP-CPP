580606635664e904eef48a3dba31d6657e3afda4
Tony <35574936+lackhole@users.noreply.github.com>
2021-02-11 18:50:20 +0900

    add non-pointer examples

--- strategy/main.cpp
+++ strategy/main.cpp
@@ -17,5 +17,15 @@ int main() {
   model->setFlyBehavior(std::make_unique<FlyRocketPowered>());
   model->performFly();
 
+  std::cout << "====================" << std::endl;
+  ModelDuck d1;
+  d1.performFly();
+  d1.setFlyBehavior(std::make_unique<FlyRocketPowered>());
+  d1.performFly();
+
+  ModelDuck d2 = d1;
+  d2.performFly();
+
+
   return 0;
 }
