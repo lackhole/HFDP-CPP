f2b87058447896341a70f9b3ae048da8ad5c9438
lackhole <35574936+lackhole@users.noreply.github.com>
2021-02-16 23:53:53 +0900

    fix cmake

---
+++ observer/weather_station/include/weather_station/weather_data/weather_data.cc
@@ -0,0 +1,5 @@
+//
+// Created by cosge on 2021-02-16.
+//
+
+#include "weather_data.h"
---
+++ observer/weather_station/include/weather_station/weather_data/weather_data.h
@@ -0,0 +1,14 @@
+//
+// Created by cosge on 2021-02-16.
+//
+
+#ifndef OBSERVER_WEATHER_DATA_H
+#define OBSERVER_WEATHER_DATA_H
+
+
+class WeatherData {
+
+};
+
+
+#endif //OBSERVER_WEATHER_DATA_H
