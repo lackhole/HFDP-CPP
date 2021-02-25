cf133fb975ae705b2b2464a8d987240e8cd94642
lackhole <35574936+lackhole@users.noreply.github.com>
2021-02-25 22:14:25 +0900

    add static_duck example

--- strategy/main.cpp
+++ strategy/main.cpp
@@ -9,35 +9,59 @@ using namespace strategy;
 
 int main() {
 
-  std::unique_ptr<dynamic_duck::AbstractDuck> mallard = std::make_unique<dynamic_duck::MallardDuck>();
+  {
+    std::cout << "\n########################################" << std::endl;
+    std::cout << "Standard dynamic-polymorphic strategy" << std::endl;
+    std::unique_ptr<dynamic_duck::AbstractDuck> mallard = std::make_unique<dynamic_duck::MallardDuck>();
 
-  mallard->performQuack();
-  mallard->performFly();
+    mallard->performQuack();
+    mallard->performFly();
 
-  std::unique_ptr<dynamic_duck::AbstractDuck> model = std::make_unique<dynamic_duck::ModelDuck>();
-  model->performFly();
-  model->setFlyBehavior(std::make_unique<dynamic_duck::FlyRocketPowered>());
-  model->performFly();
+    std::unique_ptr<dynamic_duck::AbstractDuck> model = std::make_unique<dynamic_duck::ModelDuck>();
+    model->performFly();
+    model->setFlyBehavior(std::make_unique<dynamic_duck::FlyRocketPowered>());
+    model->performFly();
 
-  model = std::make_unique<dynamic_duck::MallardDuck>();
-  model->performQuack();
+    model = std::make_unique<dynamic_duck::MallardDuck>();
+    model->performQuack();
 
-  std::cout << "====================" << std::endl;
-  dynamic_duck::ModelDuck d1;
-  d1.performFly();
-  d1.setFlyBehavior(std::make_unique<dynamic_duck::FlyRocketPowered>());
-  d1.performFly();
+    std::cout << "====================" << std::endl;
+    dynamic_duck::ModelDuck d1;
+    d1.performFly();
+    d1.setFlyBehavior(std::make_unique<dynamic_duck::FlyRocketPowered>());
+    d1.performFly();
 
-  dynamic_duck::ModelDuck d2 = d1;
-  d2.performFly();
+    dynamic_duck::ModelDuck d2 = d1;
+    d2.performFly();
 
-  // *mallard = *model; blocked!
-  try {
-    dynamic_cast<dynamic_duck::ModelDuck&>(*mallard) =
-      dynamic_cast<const dynamic_duck::ModelDuck&>(*model);
+    // *mallard = *model; blocked!
+    try {
+      dynamic_cast<dynamic_duck::ModelDuck &>(*mallard) =
+        dynamic_cast<const dynamic_duck::ModelDuck &>(*model);
+    }
+    catch (const std::exception &e) {
+      std::cout << "Bad casting" << std::endl;
+    }
   }
-  catch (const std::exception& e){
-    std::cout << "Bad casting" << std::endl;
+
+  {
+    std::cout << "\n########################################" << std::endl;
+    std::cout << "C++ least dynamic-polymorphic strategy" << std::endl;
+    static_duck::ModelDuck modelDuck;
+    modelDuck.display();
+    modelDuck.performFly();
+    modelDuck.performQuack();
+    modelDuck.setFlyBehavior(static_duck::FlyRocketPowered{});
+    modelDuck.performFly();
+
+    static_duck::MallardDuck mallardDuck;
+    mallardDuck.display();
+
+//    mallardDuck = modelDuck;
+//    mallardDuck = std::move(modelDuck);
+
+    mallardDuck.setQuackBehavior(static_duck::Squeak{});
+    mallardDuck.performQuack();
   }
 
   return 0;
